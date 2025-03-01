1277bdfbc1d09a89e79ab796952a688f00ab0721
PlanetCloud <35788716+PlanetTheCloud@users.noreply.github.com>
2025-03-02 06:23:34 +0700

    Update DefaultSchedule.js

--- jadwal/DefaultSchedule.js
+++ jadwal/DefaultSchedule.js
@@ -1474,5 +1474,158 @@ const DEFAULT_SCHEDULE = {
                 },
             ]
         }
-    }
+    },
+    "4-IFBSORE-2425": {
+        metadata: {
+            identifier: "4-IFBSORE-2425", // In case we want to make it server-based
+            major: "S-1 / Teknik Informatika",
+            class: "IF-B-SORE",
+            start: "03-03-2025", // 3 Mar 2025
+            end: "03-03-2026", // 3 Mar 2026
+            semester: 4,
+            academicYear: "2024/2025",
+            updated: "02-03-2025a"
+        },
+        schedules: {
+            regularClasses: [
+                /**
+                 * @property {string} subject Subject name
+                 * @property {int[]} day Day of the class (1 = Monday, 2 = Tuesday, so on)
+                 * @property {string} type Class type (REGULAR, REPLACEMENT, EXAM, HOLIDAY)
+                 * @property {object[]} time Hour and minute when the class starts
+                 * @property {int[]} location Where the class is being held (0 = Unknown, 1 = Onsite, 2 = Online)
+                 */
+                {
+                    subject: "Organisasi dan Arsitektur Komputer (Teori)",
+                    day: [1],
+                    type: "REGULAR",
+                    classroom: "B.T4/L4",
+                    lecturer: "Florida N.S. Damanik, S.T., M.M.",
+                    time: [
+                        {
+                            start: "17:45",
+                            end: "19:10",
+                            tolerance: "18:00",
+                        },
+                        {
+                            start: "19:30",
+                            end: "20:50",
+                            tolerance: "19:40",
+                        }
+                    ],
+                    location: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                },
+                {
+                    subject: "Pengembangan Aplikasi Mobile Front-End (Teori)",
+                    day: [2],
+                    type: "REGULAR",
+                    classroom: "B.T4/L4",
+                    lecturer: "Andy Thomas, S.Kom., M.Kom.",
+                    time: [
+                        {
+                            start: "17:45",
+                            end: "19:10",
+                            tolerance: "18:00",
+                        },
+                        {
+                            start: "19:30",
+                            end: "20:50",
+                            tolerance: "19:40",
+                        }
+                    ],
+                    location: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                },
+                {
+                    subject: "Pengembangan Perangkat Lunak Tangkas (Teori)",
+                    day: [3],
+                    type: "REGULAR",
+                    classroom: "B.T4/L4",
+                    lecturer: "Nadya Sikana, S.Kom., M.Kom.",
+                    time: [
+                        {
+                            start: "17:45",
+                            end: "19:05",
+                            tolerance: "18:00",
+                        },
+                    ],
+                    location: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                },
+                {
+                    subject: "Pengembangan Aplikasi Mobile Front-End (Praktek)",
+                    day: [3],
+                    type: "REGULAR",
+                    classroom: "A.P2/L3",
+                    lecturer: "Andy Thomas, S.Kom., M.Kom.",
+                    time: [
+                        {
+                            start: "19:20",
+                            end: "21:00",
+                            tolerance: "19:35",
+                        },
+                    ],
+                    location: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                },
+                {
+                    subject: "Sistem Operasi (Teori)",
+                    day: [4],
+                    type: "REGULAR",
+                    classroom: "B.T4/L4",
+                    lecturer: "Nurhayati, S.Kom., M.Kom.",
+                    time: [
+                        {
+                            start: "17:45",
+                            end: "19:10",
+                            tolerance: "18:00",
+                        },
+                        {
+                            start: "19:30",
+                            end: "20:50",
+                            tolerance: "19:40",
+                        }
+                    ],
+                    location: [1, 1, 2, 2, 1, 1, 2, 1, 1, 2, 1, 2, 2, 1, 1, 1],
+                },
+                {
+                    subject: "Kecerdasan Artifisial (Teori)",
+                    day: [5],
+                    type: "REGULAR",
+                    classroom: "B.T4/L4",
+                    lecturer: "Khairul Hawani Rambe, B.Sc., M.Eng.",
+                    time: [
+                        {
+                            start: "17:45",
+                            end: "19:05",
+                            tolerance: "18:00",
+                        },
+                        {
+                            start: "19:30",
+                            end: "20:50",
+                            tolerance: "19:45",
+                        },
+                    ],
+                    location: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                },
+            ],
+            events: [
+                {
+                    subject: "Istirahat",
+                    // TO DEPRECATE: Remove day 
+                    day: [1, 2, 3, 4, 5],
+                    type: "BREAK",
+                    // TO DEPRECATE: Remote time
+                    time: [
+                        {
+                            start: "19:10",
+                            end: "19:30"
+                        }
+                    ],
+                }
+            ],
+            overrides: [
+                /**
+                 * Types: HOLIDAY, EXAM, REPLACEMENT (Kelas Pengganti)
+                 */
+            ]
+        }
+    },
 }
