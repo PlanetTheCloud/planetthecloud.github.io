c84cbcebf4ad7d02cf7c375e5b928fe4e97cb8d9
PlanetCloud <35788716+PlanetTheCloud@users.noreply.github.com>
2025-06-05 14:04:46 +0700

    Update DefaultSchedule.js

--- jadwal/DefaultSchedule.js
+++ jadwal/DefaultSchedule.js
@@ -11,7 +11,7 @@ const DEFAULT_SCHEDULE = {
             end: "03-03-2026", // 3 Mar 2026
             semester: 4,
             academicYear: "2024/2025",
-            updated: "04-06-2025a"
+            updated: "05-06-2025a"
         },
         schedules: {
             regularClasses: [
@@ -378,6 +378,11 @@ const DEFAULT_SCHEDULE = {
                     subject: "Kenaikan Yesus Kristus",
                     date: "29-05-2025",
                     type: "HOLIDAY"
+                },
+                {
+                    subject: "Sistem Operasi (Teori)",
+                    date: "03-07-2025",
+                    type: "REPLACEMENT"
                 },
                 {
                     subject: "Persiapan Ujian UAS",
