50307099178c1544e88535f1b7e699cd5d2019f3
PlanetCloud <35788716+PlanetTheCloud@users.noreply.github.com>
2026-03-30 15:13:30 +0800

    Update DefaultSchedule.js

--- jadwal/DefaultSchedule.js
+++ jadwal/DefaultSchedule.js
@@ -676,11 +676,6 @@ const DEFAULT_SCHEDULE = {
             ],
             overrides: [
                 {
-                    subject: "Kriptografi dan Keamanan Informasi",
-                    type: "NOSHOW",
-                    date: "16-03-2026"
-                },
-                {
                     subject: "Pengembangan Karakter: Kewarganegaraan",
                     type: "NOSHOW",
                     date: "05-03-2026"
