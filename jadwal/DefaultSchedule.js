4e86d2855be5a59e94fa22e94f5a7936108c799c
PlanetCloud <35788716+PlanetTheCloud@users.noreply.github.com>
2026-03-30 15:13:39 +0800

    Update DefaultSchedule.js

--- jadwal/DefaultSchedule.js
+++ jadwal/DefaultSchedule.js
@@ -539,7 +539,7 @@ const DEFAULT_SCHEDULE = {
             end: "02-09-2026", // 2 Sep 2026
             semester: 6,
             academicYear: "2026/2027",
-            updated: "27-03-2026"
+            updated: "27-03-2026x"
         },
         schedules: {
             regularClasses: [
